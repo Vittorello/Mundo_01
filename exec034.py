1d7a1b80dfaf61a90e755d0f23e44b86d15ff231
Igor Camargo Vittorello <igorvittorello@hotmail.com>
2024-11-26 01:41:33 -0300

    Arrumando a linha do print

--- exec034.py
+++ exec034.py
@@ -3,6 +3,6 @@ lado2 = float(input("Segundo lado: "))
 lado3 = float(input("Terceiro lado: "))
 
 if (lado1 + lado2) > lado3 and (lado1 + lado3) > lado2 and (lado2 + lado3) > lado1:
-    print(f"{lado1}, {lado2}, {lado3} formam um triângulo.")
+    print("Os segmentos acima formam um triângulo.")
 else:
-    print(f"{lado1}, {lado2}, {lado3} não formam um triângulo.")
+    print("Os segmentos acima não formam um triângulo.")
